960a70d44081a2b0ba4c831d2791d703dd97dbce
William Casarin <william@casarin.me>
2013-09-21 13:24:35 -0700

    Support for named records

--- Pipes/Csv.hs
+++ Pipes/Csv.hs
@@ -9,38 +9,65 @@
 
 module Pipes.Csv (
   feedParser,
+  feedHeaderParser,
   decode,
-  decodeWith
+  decodeWith,
+  decodeByName,
+  decodeByNameWith
 ) where
 
 import qualified Data.Csv.Incremental as CI
 import qualified Data.ByteString as B
 
-import Data.Csv.Incremental (Parser(..))
-import Data.Csv (DecodeOptions, FromRecord, defaultDecodeOptions)
+import Data.Csv.Incremental (Parser(..), HeaderParser(..))
+import Data.Csv (DecodeOptions, FromRecord,
+                 FromNamedRecord, defaultDecodeOptions)
 import Data.ByteString (ByteString)
-import Pipes (yield, each, lift, next, Producer)
+import Pipes
 
 -- | Create a Record 'Producer' by feeding 'ByteString's into a 'Parser'
 feedParser :: Monad m
            => Parser a
            -> Producer ByteString m ()
            -> Producer (Either String a) m ()
-feedParser parser source =
-  case parser of
+feedParser parser source = case parser of
     Fail _ e  -> yield (Left e)
     Done es   -> each es
-    Some es k -> each es >> continue k source
-    Partial k -> continue k source
+    Some es k -> each es >> cont k source
+    Partial k -> cont k source
   where
-    continue k producer = do
-      x <- lift (next producer)
-      case x of
-        Left () -> feedParser (k B.empty) (return ())
-        Right (bs, producer') ->
-          if (B.null bs)
-          then continue k producer'
-          else feedParser (k bs) producer'
+    cont = continue feedParser
+
+
+-- | Create a NamedRecord 'Producer' by feeding 'ByteString's into a 'Parser'
+feedHeaderParser :: (Monad m, FromNamedRecord a)
+                 => HeaderParser (Parser a)
+                 -> Producer ByteString m ()
+                 -> Producer (Either String a) m ()
+feedHeaderParser headerParser source = case headerParser of
+    FailH bs e -> yield (Left e)
+    PartialH k -> cont k source
+    DoneH _ p  -> feedParser p source
+  where
+    cont = continue feedHeaderParser
+
+
+-- | Handle continuations properly within a Producer
+continue :: (Monad (t m), Monad m, MonadTrans t)
+         => (a -> Producer ByteString m () -> t m b)
+         -> (ByteString -> a)
+         -> Producer ByteString m ()
+         -> t m b
+continue feed k producer = do
+  x <- lift (next producer)
+  case x of
+    Left () -> feed (k B.empty) (return ())
+    Right (bs, producer') ->
+      if (B.null bs)
+      then continue feed k producer'
+      else feed (k bs) producer'
+
+
 
 -- | Equivalent to @'decodeWith' 'defaultDecodeOptions'@.
 decode :: (Monad m, FromRecord a)
@@ -51,11 +78,28 @@ decode = decodeWith defaultDecodeOptions
 
 
 -- | Create a 'Producer' that takes a 'ByteString' 'Producer' as input,
--- producing either errors or CSV records.
+-- producing either errors or 'FromRecord's.
 decodeWith :: (Monad m, FromRecord a)
            => DecodeOptions
            -> Bool
            -> Producer ByteString m ()
            -> Producer (Either String a) m ()
 decodeWith opts skipHeader src = feedParser (CI.decodeWith opts skipHeader) src
+
+
+-- | Equivalent to @'decodeByNameWith' 'defaultDecodeOptions'@.
+decodeByName :: (Monad m, FromNamedRecord a)
+             => Producer ByteString m ()
+             => Producer (Either String a) m ()
+decodeByName = decodeByNameWith defaultDecodeOptions
+
+
+-- | Create a 'Producer' that takes a 'ByteString' 'Producer' as input,
+-- producing either errors or 'FromNamedRecord's.
+decodeByNameWith :: (Monad m, FromNamedRecord a)
+                 => DecodeOptions
+                 -> Producer ByteString m ()
+                 -> Producer (Either String a) m ()
+decodeByNameWith opts src = feedHeaderParser (CI.decodeByNameWith opts) src
+
 
--- test/stdin/Stdin.hs
+++ test/stdin/Stdin.hs
@@ -1,13 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
 
-import Pipes.Csv (decode)
+import Pipes.Csv (decode, decodeByName)
 import Pipes.ByteString (stdin, ByteString)
-import Data.Csv (Record)
+import Data.Csv ((.:), FromNamedRecord(..), Record)
 import Pipes
+import Control.Applicative
 
-decoder :: Monad m => Producer ByteString m () -> Producer (Either String Record) m ()
+data Person = Person String Int
+            deriving (Show)
+
+instance FromNamedRecord Person where
+  parseNamedRecord p =
+    Person <$> p .: "name"
+           <*> p .: "age"
+
+decoder :: Monad m
+        => Producer ByteString m ()
+        -> Producer (Either String Record) m ()
 decoder = decode False
 
-decoder2 :: Monad m => Producer ByteString m () -> Producer (Either String (Int, Int)) m ()
+decoder2 :: Monad m
+         => Producer ByteString m ()
+         -> Producer (Either String (Int, Int)) m ()
 decoder2 = decode False
 
-main = runEffect $ for (decoder stdin) (lift . print)
+persons :: Monad m
+        => Producer ByteString m ()
+        -> Producer (Either String Person) m ()
+persons = decodeByName
+
+main = runEffect $ for (persons stdin) (lift . print)
