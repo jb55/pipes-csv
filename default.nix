d98a032d6770b3553949746da895b20cae5872bc
William Casarin <bill@casarin.me>
2015-09-20 15:47:26 -0700

    remove nix files

--- default.nix
+++
@@ -1,2 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./pipes-csv.nix { }
--- shell.nix
+++
@@ -1,2 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
